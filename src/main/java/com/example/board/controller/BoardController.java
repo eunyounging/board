5844333564dad4a57a87b3fb428136363371e196
“eunyoung” <jyy714@gmail.com>
2025-01-20 21:35:14 +0900

    feat: 특정 게시글 삭제 기능

--- src/main/java/com/example/board/controller/BoardController.java
+++ src/main/java/com/example/board/controller/BoardController.java
@@ -9,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -62,5 +63,13 @@ public class BoardController {
         BoardWithAgeResponseDto boardWithAgeResponseDto = boardService.findById(id);
 
         return new ResponseEntity<>(boardWithAgeResponseDto, HttpStatus.OK);
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+
+        boardService.delete(id);
+
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
